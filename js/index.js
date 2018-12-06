93080291e9eed8a2f684bade206747f1c7057ea2
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-06 01:25:57 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+var element = document.createElement('div');
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+
+};
+
+
+var appendChild()
+element.style.textAlign = 'center'
+document.body.appendChild(element);
+ul.style.textAlign = 'left';
+;
+
+var removeChild
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+;
+
+var element.remove()
+ul.remove();
+;,
